7aa8ae87fa9f21135d3914bd74e811dc10401b4a
Mavile Bekirova <mavilebekirova@Air-Mavile.Dlink>
2023-01-26 15:01:34 +0300

    task 19

--- vue3/src/App.vue
+++ vue3/src/App.vue
@@ -2,17 +2,30 @@
 export default{
 data(){
   return{
-    hidden: true,
-    hidden1: false,
+    visible: true,
+btnHide: true,
+btnVis: false,
   }
 },
+methods: {
+  hide: function(){
+    this.visible = false;
+    this.btnHide = false;
+    this.btnVis = true;
+  },
+  vis: function(){
+    this.visible = true;
+    this.btnHide = true;
+    this.btnVis = false;
+  }
+}
 }
 </script>
 
 <template>
-<p v-if="hidden">text</p>
-<p v-if="!hidden1">text</p>
-
+<button @click="hide" v-if="btnHide">hide</button>
+<button @click="vis" v-if="btnVis">visible</button>
+<p v-if="visible">text</p>
 </template>
 
 <style scoped>
