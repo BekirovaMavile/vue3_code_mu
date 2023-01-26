c416eec34cb1175afe52c4d363ef5d7bcf5ff445
Mavile Bekirova <mavilebekirova@Air-Mavile.Dlink>
2023-01-26 16:33:41 +0300

    task 23

--- vue3/src/App.vue
+++ vue3/src/App.vue
@@ -2,7 +2,7 @@
 export default{
 data(){
   return{
-    isAdmin: true,
+    day: 'Пятница',
   }
 },
 methods: {
@@ -12,8 +12,13 @@ methods: {
 </script>
 
 <template>
-<p v-if="isAdmin">Hey!!!</p>
-<p v-else>Admin false</p>
+<p v-if="day === 'Понедельник'">Понедельник</p>
+<p v-if="day === 'Вторник'">Вторник</p>
+<p v-if="day === 'Среда'">Среда</p>
+<p v-if="day === 'Четверг'">Четверг</p>
+<p v-if="day === 'Пятница'">Пятница</p>
+<p v-if="day === 'Суббота'">Суббота</p>
+<p v-if="day === 'Воскресенье'">Воскресенье</p>
 </template>
 
 <style scoped>
