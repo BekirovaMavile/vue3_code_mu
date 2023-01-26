0b3ec9614776fd0273371324a48143bdc90bf7d4
Mavile Bekirova <mavilebekirova@Air-Mavile.Dlink>
2023-01-26 15:23:31 +0300

    task 22

--- vue3/src/App.vue
+++ vue3/src/App.vue
@@ -2,32 +2,18 @@
 export default{
 data(){
   return{
-    visible: true,
-    visible1: true,
-    visible2: true,
+    isAdmin: true,
   }
 },
 methods: {
-  toggle: function(){
-    this.visible = !this.visible;
-  },
-  toggle1: function () {
-    this.visible1 = !this.visible1;
-  },
-  toggle2: function () {
-    this.visible2 = !this.visible2;
-  }
+
 }
 }
 </script>
 
 <template>
-<button @click="toggle">toggle</button>
-{{visible ? 'hide' : 'show'}}
-<button @click="toggle1">toggle</button>
-{{visible1 ? 'hey' : 'bye'}}
-<button @click="toggle2">toggle</button>
-{{visible2 ? 'yes' : 'no'}}
+<p v-if="isAdmin">Hey!!!</p>
+<p v-else>Admin false</p>
 </template>
 
 <style scoped>
