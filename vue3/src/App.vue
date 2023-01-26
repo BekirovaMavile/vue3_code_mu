35ae869d5191377602f96acaa02c708ff939678d
Mavile Bekirova <mavilebekirova@Air-Mavile.Dlink>
2023-01-26 18:42:53 +0300

    task 28

--- vue3/src/App.vue
+++ vue3/src/App.vue
@@ -2,7 +2,7 @@
 export default{
 data(){
   return{
-    items: [1, 2, 3, 4, 5],
+    arr: ['x', 'y', 'z'],
   }
 },
 methods: {
@@ -12,13 +12,7 @@ methods: {
 </script>
 
 <template>
-  <div v-for="elem in items">{{ elem }}</div>
-  <br>
-  <br>
-  <div v-for="elem in items">{{ elem*elem }}</div>
-  <br>
-  <br>
-  <ul v-for="elem in items">{{ elem }}</ul>
+  <ul v-for="(elem, key) in arr"> {{ key }} {{ elem }}</ul>
 </template>
 
 <style scoped>
