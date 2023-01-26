81c6f2fb1cff39f903095d43f11226f0142ffd65
Mavile Bekirova <mavilebekirova@Air-Mavile.Dlink>
2023-01-26 15:18:50 +0300

    task 21

--- vue3/src/App.vue
+++ vue3/src/App.vue
@@ -3,19 +3,19 @@ export default{
 data(){
   return{
     visible: true,
+    visible1: true,
     visible2: true,
-    visible3: true,
   }
 },
 methods: {
   toggle: function(){
     this.visible = !this.visible;
   },
+  toggle1: function () {
+    this.visible1 = !this.visible1;
+  },
   toggle2: function () {
     this.visible2 = !this.visible2;
-  },
-  toggle3: function () {
-    this.visible3 = !this.visible3;
   }
 }
 }
@@ -23,11 +23,11 @@ methods: {
 
 <template>
 <button @click="toggle">toggle</button>
-<p v-if="visible">111</p>
+{{visible ? 'hide' : 'show'}}
+<button @click="toggle1">toggle</button>
+{{visible1 ? 'hey' : 'bye'}}
 <button @click="toggle2">toggle</button>
-<p v-if="visible2">222</p>
-<button @click="toggle3">toggle</button>
-<p v-if="visible3">333</p>
+{{visible2 ? 'yes' : 'no'}}
 </template>
 
 <style scoped>
