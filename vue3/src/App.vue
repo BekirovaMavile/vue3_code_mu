cb2777cc9351db605964473d1863c05ec550d6aa
Mavile Bekirova <mavilebekirova@Air-Mavile.Dlink>
2023-01-27 16:54:07 +0300

    task 4

--- vue3/src/App.vue
+++ vue3/src/App.vue
@@ -10,36 +10,10 @@ data(){
 </script>
 
 <template>
-  <div>{{ name }}</div>
-  <div>{{ surname }}</div>
-  <!-- <div>Hello World</div> -->
+  <div class="hunter">{{ name }}</div>
+  <div class="hunter">{{ surname }}</div>
 </template>
 
 <style scoped>
-/* header {
-  line-height: 1.5;
-}
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-} */
 </style>
