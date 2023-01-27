95b92f139c10a51e828792b5d820461ba773acf0
Mavile Bekirova <mavilebekirova@Air-Mavile.Dlink>
2023-01-27 18:49:14 +0300

    task 13

--- vue3/src/App.vue
+++ vue3/src/App.vue
@@ -16,35 +16,11 @@ methods: {
 </script>
 
 <template>
-<button @click="show(2)">btn1</button>
-<button @click="show1(3)">btn2</button>
+<button class="button" @click="show(2)">btn1</button>
+<button class="button
+" @click="show1(3)">btn2</button>
 </template>
 
 <style scoped>
-/* header {
-  line-height: 1.5;
-}
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-} */
 </style>
