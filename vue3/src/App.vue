d26b286fc286801e78607811177f830f33d0ae28
Mavile Bekirova <mavilebekirova@Air-Mavile.Dlink>
2023-01-27 18:53:15 +0300

    task 14

--- vue3/src/App.vue
+++ vue3/src/App.vue
@@ -17,9 +17,9 @@ change2: function(){
 </script>
 
 <template>
-<p>{{ text }}</p>
-<button @click="change">text1</button>
-<button @click="change2">text2</button>
+<p class="hunter">{{ text }}</p>
+<button class="button" @click="change">text1</button>
+<button class="button" @click="change2">text2</button>
 </template>
 
 <style scoped>
