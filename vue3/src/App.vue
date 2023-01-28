9d3b3cb3f6bbd49820149341c2ace76359a8530a
Mavile Bekirova <mavilebekirova@Air-Mavile.Dlink>
2023-01-28 12:41:22 +0300

    task 24

--- vue3/src/App.vue
+++ vue3/src/App.vue
@@ -12,9 +12,9 @@ methods: {
 </script>
 
 <template>
-<p v-if="age < 18">Подросток</p>
-<p v-else-if="19 < age && age < 25">Молодой человек</p>
-<p v-else="age > 26">Мужчина</p>
+<p class="hunter" v-if="age < 18">Подросток</p>
+<p class="hunter" v-else-if="19 < age && age < 25">Молодой человек</p>
+<p class="hunter" v-else="age > 26">Мужчина</p>
 </template>
 
 <style scoped>
