9ffbd806c22af21eeb20dbcc1435761e655ca992
Mavile Bekirova <mavilebekirova@Air-Mavile.Dlink>
2023-01-28 14:46:51 +0300

    task 39

--- vue3/src/App.vue
+++ vue3/src/App.vue
@@ -2,6 +2,7 @@
 export default{
 data(){
   return{
+    visible: true,
     styles: {
       hidden: true,
     },
@@ -10,15 +11,23 @@ data(){
 methods: {
 setDone:function(){
   this.styles.hidden = false;
-}
+},
+setTwo: function(){
+  this.styles.hidden = true;
+},
+  toggle: function () {
+    this.visible = !this.visible;
+  }
 }
 }
 </script>
 
 <template>
-<p class="hunter">Shadowhunter</p>
+<button class="button" @click="toggle">toggle</button>
+<p class="hunter" v-if="visible">Shadowhunter</p>
 <p :class="styles">Conclave</p>
-<button @click="setDone">hide</button>
+<button class="button" @click="setDone">visible</button>
+<button class="button" @click="setTwo">hide</button>
 </template>
 
 <style scoped>
