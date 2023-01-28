59a0030bbed1f52645ae2db69f6ca3feb6b35527
Mavile Bekirova <mavilebekirova@Air-Mavile.Dlink>
2023-01-28 14:04:01 +0300

    task 36

--- vue3/src/App.vue
+++ vue3/src/App.vue
@@ -2,47 +2,18 @@
 export default{
 data(){
   return{
-    hunters: ['Alec', 'Isabel', 'Max'],
+    
   }
 },
 methods: {
-add: function(){
-  this.hunters.push('Jace');
-},
-del: function(){
-  this.hunters.shift();
-},
-del1: function () {
-  this.hunters.pop();
-},
-splice: function(){
-  this.hunters.splice(2, 1);
-},
-sort: function(){
-  this.hunters.sort();
-},
-  reverse: function () {
-    this.hunters.reverse();
-  },
+
 }
 }
 </script>
 
 <template>
-<ul class="styled">
-  <li v-for="elem in hunters">{{ elem }}</li>
-</ul>
-<button class="button" @click="add">add hunter</button>
-<br>
-<button class="button" @click="del">delete hunter</button>
-<br>
-<button class="button" @click="del1">delete hunter at the end</button>
-<br>
-<button class="button" @click="splice">delete the penultimate hunter</button>
-<br>
-<button class="button" @click="sort">sort array</button>
-<br>
-<button class="button" @click="reverse">array in reverse order</button>
+<p class="hunter">Alec Lightwood</p>
+<p class="hunter">Jace Herondale</p>
 </template>
 
 <style scoped>
