9719fb4f76dda0600141feb33e6a799c0026578b
Mavile Bekirova <mavilebekirova@Air-Mavile.Dlink>
2023-01-28 15:14:42 +0300

    task 40

--- vue3/src/App.vue
+++ vue3/src/App.vue
@@ -2,27 +2,26 @@
 export default{
 data(){
   return{
-    styles: {
-      hidden: true,
-    },
+    
   }
 },
 methods: {
-setDone:function(){
-  this.styles.hidden = false;
-}
+
 }
 }
 </script>
 
 <template>
-<p class="hunter">Shadowhunter</p>
-<p :class="styles">Conclave</p>
-<button @click="setDone">hide</button>
+<p :class="{active: true, error: false}">Conclave</p>
 </template>
 
 <style scoped>
-p.hidden{
-  display: none;
+p.active{
+padding: 10px;
+  margin: 10px;
+  background: #08C;
+  color: white;
+  font-weight: bold;
+  text-decoration: none;
 }
 </style>
