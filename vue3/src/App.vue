c0f6243bdf442a2bfbd0f0f5916bdbfb206b4a37
Mavile Bekirova <mavilebekirova@Air-Mavile.Dlink>
2023-01-29 15:55:15 +0300

    task 49

--- vue3/src/App.vue
+++ vue3/src/App.vue
@@ -2,7 +2,7 @@
 export default{
 data(){
   return{
-    language: '',
+    sity: 'Симферополь',
   }
 },
 methods: {
@@ -12,24 +12,13 @@ methods: {
 </script>
 
 <template>
-  <div class="hunter">
-    <p>Какой язык ваш родной?</p>
-<input name="radio" type="radio" v-model="language" value="Крымскотатарский">
-<label for="">Крымскотатарский</label>
-<br>
-<input name="radio" type="radio" v-model="language" value="Русский">
-<label for="">Русский</label>
-<br>
-<input name="radio" type="radio" v-model="language" value="Испанский">
-<label for="">Испанский</label>
-</div>
-<!-- <p v-if="value === 'Крымскотатарский'">Хош кельдинъиз</p>
-<p v-else-if="value === 'Русский'">Добро пожаловать</p>
-<p v-else-if="value === 'Испанский'">Bienvenido</p>
-<p v-else=''> </p> -->
-
-<p>Ваш выбор: {{ language }}</p>
-<br>
+  <p class="hunter">В каком городе вы живете?</p>
+    <select class="hunter" v-model="sity">
+      <option>Симферополь</option>
+      <option>Севастополь</option>
+      <option>Ялта</option>
+    </select>
+    <p>{{ sity }}</p>
 </template>
 
 <style scoped>
